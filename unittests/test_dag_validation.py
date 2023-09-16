6032b26c480298a8f2d9949dcbfd0a4afb153dde
airflow-workstation <ec2-user@ip-172-31-74-226.ec2.internal>
2023-09-16 19:01:17 +0000

    comment label

--- unittests/test_dag_validation.py
+++ unittests/test_dag_validation.py
@@ -17,12 +17,12 @@ class TestDagValidation:
         assert len(dagbag.import_errors) == 0, "DAG failures detected! Got: {}".format(
             dagbag.import_errors
         )
-    """
+    
     def test_time_import_dags(self, dagbag):
         """
-    #        Verify that DAGs load fast enough
-    #       - check for loading time
-    """
+            Verify that DAGs load fast enough
+           - check for loading time
+        """
         stats = dagbag.dagbag_stats
         slow_dags = list(filter(lambda f: f.duration > self.LOAD_SECOND_THRESHOLD, stats))
         res = ', '.join(map(lambda f: f.file[1:], slow_dags))        
@@ -30,7 +30,7 @@ class TestDagValidation:
         assert len(slow_dags) == 0, "The following DAGs take more than {0}s to load: {1}".format(
             self.LOAD_SECOND_THRESHOLD,
             res
-        ) """
+        ) 
     
     def test_default_args_retries(self, dagbag):
         """
